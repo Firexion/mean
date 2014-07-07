c7423640f90cffb476bed57e406010ea48f1d833
Sam Perdue <firexion@gmail.com>
2014-07-06 22:25:03 -0400

    fixed modal display message

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -8,34 +8,31 @@ angular.module('yoMinesweeperApp')
     $scope.reveal = function(spot) {
       if ($scope.game.revealSpot(spot)) {
       	var gameOverModal = $modal.open({
-        templateUrl: $scope.gameOver,
-        controller: GameOverCtrl,
-        resolve: {
-          won: function () {
-            return $scope.game.won;
-          }
-        }
-     });
+	        templateUrl: $scope.gameOver,
+	        controller: GameOverCtrl,
+	        resolve: {
+	          won: function () {
+	            return $scope.game.won;
+	          }
+	        }
+     		});
 
-     gameOverModal.result.then(function() {
-       $scope.game.reset();
-     }, function () {
-         $log.info('Modal dismissed at: ' + new Date());
-     });
-      }
+	     	gameOverModal.result.then(function() {
+	        $scope.game.reset();
+	     	}, function () {
+	        $log.info('Modal dismissed at: ' + new Date());
+	     	});
+  		}
     }
-
   });
 
 
 var GameOverCtrl = function ($scope, $modalInstance, won) {
-	console.log(won);
+	$scope.user = {name: 'guest', last: 'visitor'};
+  $scope.message = "You lose. \nPlease try again.";
   if (won) {
-    $scope.won = "You win! \nGreat game.";
-  } else {
-    $scope.won = "You lose. \nPlease try again.";
+    $scope.message = "You win! \nGreat game.";
   }
-
 
   $scope.ok = function () {
     $modalInstance.close();
